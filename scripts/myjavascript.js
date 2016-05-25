5eff5e5d3d2e6ac8814d0572ee13bc52d7a1f6cf
Maetz007 <maetz007@yahoo.com>
2016-05-25 08:32:00 -0500

    ready

--- scripts/myjavascript.js
+++ scripts/myjavascript.js
@@ -1,5 +1,28 @@
-var getElementNum = function(){
-  for(var i = 0; i < arrayNeedsName.length; i++){
 
-  }
+var randomColor = function(){
+  var number = Math.floor(Math.random() * 4);
+
+    if(number === 0){
+    console.log("Red");
+    document.getElementById("newColor").style.color = "red";
+    document.getElementById("newColor").innerHTML = "RED";
+    }
+
+    else if (number == 1){
+    console.log("Blue");
+    document.getElementById("newColor").style.color = "blue";
+    document.getElementById("newColor").innerHTML = "BLUE";
+    }
+
+    else if (number == 2){
+    console.log("Green");
+    document.getElementById("newColor").style.color = "green";
+    document.getElementById("newColor").innerHTML = "GREEN";
+    }
+
+    else {
+    console.log("Yellow");
+    document.getElementById("newColor").style.color = "yellow";
+    document.getElementById("newColor").innerHTML = "YELLOW";
+    }
 };
